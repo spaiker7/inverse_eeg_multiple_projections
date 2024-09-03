4355f4043b7f1e194a5ca1e384f417937c9c307a
Victor Kozhevnikov <70488161+spaiker7@users.noreply.github.com>
2024-09-03 20:27:47 +0300

    reading h5 synthesized archive

--- gen/project_and_interpolate.py
+++ gen/project_and_interpolate.py
@@ -11,7 +11,7 @@ from scipy.interpolate import griddata
 
 mne.viz.set_3d_backend('pyvistaqt')
 
-from utils import crop_img_by_zeros, norm_data
+from utils import crop_img_by_zeros
 
 class CorticalProjectionPreprocessor:
     """
@@ -71,10 +71,13 @@ class CorticalProjectionPreprocessor:
                                 ['lh', 'lateral'], ['lh', 'medial'], ['rh', 'lateral'], ['rh', 'medial']]
         
         num_subjects = len(list(self.config["subj_dict"].keys()))
-        self.archive_name = Path(self.config["gen_dir"]) / f'topomaps_cortex-views_subj-{num_subjects}_ico{self.config["spacing_ico"]}_lbl-{self.n_labels}.h5'
+
+        self.in_archive_name = Path(self.config["gen_dir"]) / \
+            f'sensors_dipoles_subj-{num_subjects}_ico{self.config["spacing_ico"]}_lbl-{self.n_labels}.h5'
+        self.out_archive_name = Path(self.config["gen_dir"]) / \
+            f'topomaps_cortex-views_subj-{num_subjects}_ico{self.config["spacing_ico"]}_lbl-{self.n_labels}.h5'
 
         self.fwds = {}
-        self.save_dirs = {}
 
         for subject in self.config["subj_dict"].keys():
 
@@ -84,58 +87,49 @@ class CorticalProjectionPreprocessor:
             fwd_path = Path(self.config["fwd_dir"]) / fwd_fname
 
             self.fwds[subject] = mne.read_forward_solution(fwd_path)
-            self.save_dirs[subject] = Path(self.config["gen_dir"]) / fwd_fname.replace(".fif","") / f'Parcellation_{self.n_labels}-lbl'
             
 
         if self.onto_morph:
 
             self.loss_masks = get_cortical_loss_masks(self.config, self.hemi_view_combinations)
 
-
-        with h5py.File(self.archive_name, 'a') as out_file:
+        with h5py.File(self.in_archive_name, 'r') as in_file, \
+            h5py.File(self.out_archive_name, 'a') as out_file:
             
-            keys = list(out_file.keys())
+            for subj in self.config["subj_dict"].keys():
+                
+                if subj not in out_file['cortex-views'].keys():
+                    out_file.create_group(f'cortex-views/{subj}')
+                if subj not in out_file['topomaps'].keys():
+                    out_file.create_group(f'topomaps/{subj}')
 
-            for j, subject in enumerate(self.config["subj_dict"].keys()):
-
-                fwd = self.fwds[subject]
+                fwd = self.fwds[subj]
                 src = fwd['src']
                 vertices = [src[0]['vertno'], src[1]['vertno']]
-                save_dir = self.save_dirs[subject]
 
-                sensors_dir = save_dir / "sensors"
-                dipoles_dir = save_dir / "dipoles"
+                print(f"\nGenerating topomaps and cortical views for {subj} subject:")
 
-                print(f"\nGenerating topomaps and cortical views for {subject} subject:")
-
-                for sensors_name, source_name in tqdm(list(zip((sensors_dir.iterdir()), dipoles_dir.iterdir()))):
+                for sensors_name, dipoles_name in tqdm(list(zip(list(in_file[f'sensors/{subj}']), list(in_file[f'dipoles/{subj}'])))):
                     
-                    i = int(sensors_name.stem.split("_")[0].split("-")[-1])
-                    n_lbls = source_name.stem.split("_")[-1].split("-")[-1]
-                    snr = sensors_name.stem.split("_")[-1].split("-")[-1]
+                    topomaps_name = '-'.join(sensors_name.split('-')[1:])
+                    cortex_views_name = '-'.join(dipoles_name.split('-')[1:])
 
-                    cortex_views_name = f'cortex_views_dip-{j*10000+i}_lbls-{n_lbls}_subj-{subject}'
-                    topomaps_name = f'topomaps_eeg-{j*10000+i}_snr-{snr}_subj-{subject}'
-
-                    if topomaps_name not in keys:
-                        topomaps = self.project_and_interpolate_sensors(sensors_name)
-                        out_file.create_dataset(topomaps_name, data=topomaps,
-                                            shape=topomaps.shape, dtype=np.float32, compression="gzip", compression_opts=4)
+                    if topomaps_name not in out_file['topomaps'][subj].keys():
+                        topomaps = self.project_and_interpolate_sensors(in_file, subj, sensors_name)
+                        out_file.create_dataset(f'topomaps/{subj}/{topomaps_name}', data=topomaps,
+                                            shape=topomaps.shape, dtype=np.float16, compression="gzip", compression_opts=5)
                     
-                    if cortex_views_name not in keys:
-                        cortex_views = self.project_sources_onto_cortex(subject, source_name, vertices)
-                        out_file.create_dataset(cortex_views_name, data=cortex_views,
-                                        shape=cortex_views.shape, dtype=np.float32, compression="gzip", compression_opts=4)
+                    if cortex_views_name not in out_file['cortex-views'][subj].keys():
+                        cortex_views = self.project_sources_onto_cortex(in_file, subj, dipoles_name, vertices)
+                        out_file.create_dataset(f'cortex-views/{subj}/{cortex_views_name}', data=cortex_views,
+                                        shape=cortex_views.shape, dtype=np.float16, compression="gzip", compression_opts=5)
                         
         print("Done!")
 
 
-    def project_and_interpolate_sensors(self, sensors_path, sep_hyperplanes_coords=[0, -20, 0], normalize=True):
+    def project_and_interpolate_sensors(self, in_file, subj, sensors_name, sep_hyperplanes_coords=[0, -20, 0]):
 
-        sensors = np.load(sensors_path)
-        if normalize:
-            sensors = norm_data(sensors)
-
+        sensors = np.array(in_file[f'sensors/{subj}/{sensors_name}'])
         x, y, z = sep_hyperplanes_coords
 
         if self.onto_morph:
@@ -173,25 +167,26 @@ class CorticalProjectionPreprocessor:
         topomaps['frontal'] = cv2.rotate(topomaps['frontal'], cv2.ROTATE_180)
 
         topomaps = np.nan_to_num(np.stack(topomaps.values()))
-        topomaps = np.array(topomaps, dtype=np.float32)
+        topomaps = np.array(topomaps, dtype=np.float16)
 
         return topomaps
 
 
-    def project_sources_onto_cortex(self, subject, sources_path, vertices):
+    def project_sources_onto_cortex(self, in_file, subj, dipoles_name, vertices):
         
-        subject_name = self.config["subj_dict"][subject]["fs_name"]
-        sources = np.load(sources_path)
+        subject_name = self.config["subj_dict"][subj]["fs_name"]
+        sources = np.array(in_file[f'dipoles/{subj}/{dipoles_name}'])
         stc = mne.SourceEstimate(sources, vertices, subject=subject_name, tmin=0, tstep=1/1000)
 
         if self.onto_morph:
-            morph_path = Path(self.config["morph_dir"]) / f'{subject_name}-fsaverage-ico{self.config["spacing_ico"]}.h5'
+            morph_path = Path(self.config["morph_dir"]) / \
+                f'{subject_name}-fsaverage-ico{self.config["spacing_ico"]}.h5'
             morph = mne.read_source_morph(morph_path)
             stc = morph.apply(stc)
 
         views = []
 
-        Path('./temp').mkdir(parents=True, exist_ok=True)
+        Path('./tmp').mkdir(parents=True, exist_ok=True)
         for hemi_view_combination, loss_mask in zip(self.hemi_view_combinations, self.loss_masks):
 
             brain = stc.plot(
@@ -201,7 +196,7 @@ class CorticalProjectionPreprocessor:
                 size=(500, 500),
                 smoothing_steps=4,
                 time_viewer=False,
-                clim={'kind': 'value', 'pos_lims':(0.01, 0.6, 1)},
+                clim={'kind': 'value', 'pos_lims':(0., 500.0, 1000.0)},
                 colorbar=False,
                 colormap='gray',
                 cortex=(0, 0, 0),
@@ -211,21 +206,22 @@ class CorticalProjectionPreprocessor:
             screenshot = brain.screenshot()
             brain.close()
 
-            temp_path = 'temp/' + '_'.join(hemi_view_combination) + '.png'
-            cv2.imwrite(temp_path, screenshot)
+            tmp_path = 'tmp/' + '_'.join(hemi_view_combination) + '.png'
+            cv2.imwrite(tmp_path, screenshot)
 
-            view = cv2.imread(temp_path, cv2.IMREAD_GRAYSCALE)
+            view = cv2.imread(tmp_path, cv2.IMREAD_GRAYSCALE)
             view = crop_img_by_zeros(view, by_img=loss_mask)
             view = cv2.resize(view, (self.grid,self.grid))
 
             views.append(view)
 
-        views = np.array(views, dtype=np.float32)/255
+        views = np.array(views, dtype=np.float16)/255*100
 
         return views
 
 
-def get_cortical_loss_masks(config, hemi_view_combinations, subject='fsaverage', save_dir='', fwd=None, save=True, grid=256):
+def get_cortical_loss_masks(config, hemi_view_combinations, subject='fsaverage', save_dir='', 
+                            fwd=None, save=True, grid=256):
     """
     Calculates cortcial projections' masks (hemi view combinations).
     """
@@ -244,7 +240,7 @@ def get_cortical_loss_masks(config, hemi_view_combinations, subject='fsaverage',
     loss_masks = []
     uncroppped_loss_masks = []
 
-    Path('./temp').mkdir(parents=True, exist_ok=True)
+    Path('./tmp').mkdir(parents=True, exist_ok=True)
     for hemi_view_combination in hemi_view_combinations:
         brain = stc.plot(
             subjects_dir=config["subjects_dir"],
@@ -263,10 +259,10 @@ def get_cortical_loss_masks(config, hemi_view_combinations, subject='fsaverage',
         screenshot = brain.screenshot()
         brain.close()
 
-        temp_path = 'temp/' + '_'.join(hemi_view_combination) + '.png'
-        cv2.imwrite(temp_path, screenshot)
+        tmp_path = 'tmp/' + '_'.join(hemi_view_combination) + '.png'
+        cv2.imwrite(tmp_path, screenshot)
 
-        view = cv2.imread(temp_path, cv2.IMREAD_GRAYSCALE)
+        view = cv2.imread(tmp_path, cv2.IMREAD_GRAYSCALE)
         view[view>0] = 1
 
         cropped_view = crop_img_by_zeros(view)
