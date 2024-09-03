788a3f91c907212f803f4e2372e5713799b476b3
Victor Kozhevnikov <70488161+spaiker7@users.noreply.github.com>
2024-09-03 20:29:20 +0300

    adding spatial+channel attention (CBAM) instead of attention gates (AG)

--- model/models.py
+++ model/models.py
@@ -2,10 +2,43 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        self.fc = nn.Sequential(
+            nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False),
+            nn.ReLU(),
+            nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
+        )
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc(self.avg_pool(x))
+        max_out = self.fc(self.max_pool(x))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=kernel_size // 2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
 
 class DoubleConv(nn.Module):
-    """(convolution => [BN] => Tanh) * 2"""
-
     def __init__(self, in_channels, out_channels, mid_channels=None):
         super().__init__()
         if not mid_channels:
@@ -19,29 +52,17 @@ class DoubleConv(nn.Module):
             nn.ReLU(inplace=True)
         )
 
+        self.ch_attention = ChannelAttention(out_channels)
+        self.spatial_attention = SpatialAttention()
+
     def forward(self, x):
-        return self.double_conv(x)
+        x = self.double_conv(x)
+        x = self.ch_attention(x) * x
+        x = self.spatial_attention(x) * x
+        return x
 
-class AttentionGate(nn.Module):
-    def __init__(self, in_channels_g, in_channels_l, out_channels):
-        super(AttentionGate, self).__init__()
-        self.W_g = nn.Conv2d(in_channels_g, out_channels, kernel_size=1, bias=False)
-        self.W_x = nn.Conv2d(in_channels_l, out_channels, kernel_size=1, bias=True)
-        self.psi = nn.Conv2d(out_channels, 1, kernel_size=1, bias=True)
-        self.relu = nn.ReLU(inplace=True)
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, g, x):
-        g1 = self.W_g(g)
-        x1 = self.W_x(x)
-        psi = self.relu(g1 + x1)
-        psi = self.psi(psi)
-        alpha = self.sigmoid(psi)
-        return x * alpha
 
 class Down(nn.Module):
-    """Downscaling with maxpool then double conv"""
-
     def __init__(self, in_channels, out_channels):
         super(Down, self).__init__()
         self.maxpool_conv = nn.Sequential(
@@ -52,31 +73,24 @@ class Down(nn.Module):
     def forward(self, x):
         return self.maxpool_conv(x)
 
+
 class Up(nn.Module):
-    """Upscaling then double conv"""
-
-    def __init__(self, in_channels, out_channels, attention=True):
+    def __init__(self, in_channels, out_channels):
         super(Up, self).__init__()
-
         self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        self.attention = attention
-        if attention:
-            self.attention_gate = AttentionGate(in_channels // 2, in_channels // 2, in_channels // 4)
-        self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+        self.conv = DoubleConv(in_channels, out_channels)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
-        # input is CHW
         diffY = x2.size()[2] - x1.size()[2]
         diffX = x2.size()[3] - x1.size()[3]
 
         x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-                       diffY // 2, diffY - diffY // 2])
+                        diffY // 2, diffY - diffY // 2])
 
-        if self.attention:
-            x2 = self.attention_gate(x2, x1)
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
+
 
 class OutConv(nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -88,9 +102,10 @@ class OutConv(nn.Module):
         x = self.conv(x)
         return self.out_relu(x)
 
-class AttentionUNet(nn.Module):
+
+class UNetCBAM(nn.Module):
     def __init__(self, in_channels=5, out_channels=7):
-        super(AttentionUNet, self).__init__()
+        super(UNetCBAM, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
 
@@ -99,23 +114,25 @@ class AttentionUNet(nn.Module):
         self.down2 = Down(128, 256)
         self.down3 = Down(256, 512)
         self.down4 = Down(512, 512)
-        self.up1 = Up(1024, 256, attention=True)
-        self.up2 = Up(512, 128, attention=True)
-        self.up3 = Up(256, 64, attention=True)
-        self.up4 = Up(128, 64, attention=True)
+        self.up1 = Up(1024, 256)
+        self.up2 = Up(512, 128)
+        self.up3 = Up(256, 64)
+        self.up4 = Up(128, 64)
         self.outc = OutConv(64, out_channels)
 
     def forward(self, x):
         x1 = self.inc(x)
+
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
+
         x = self.up1(x5, x4)
         x = self.up2(x, x3)
         x = self.up3(x, x2)
         x = self.up4(x, x1)
+        
         x = self.outc(x)
 
         return x
-
